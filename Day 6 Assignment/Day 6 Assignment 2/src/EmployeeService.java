db2d120f3db6cba397e1365eaecd986a2a750cda
arpitsaxena16 <30439383+arpitsaxena16@users.noreply.github.com>
2017-08-01 14:24:12 +0530

    Update EmployeeService.java

--- Day 6 Assignment/Day 6 Assignment 2/src/EmployeeService.java
+++ Day 6 Assignment/Day 6 Assignment 2/src/EmployeeService.java
@@ -1,10 +1,18 @@
 /**
+ * Arpit's Comments: 
+ * 1) There are not commnets on methods, all methods other than getters and setters should have comments on them to explain the functionality.
+ */
+/**
  * Created by katkhedd on 7/25/2017.
  */
 public class EmployeeService {
 
     private final AccountService accountService = new AccountService();
     private final Manager manager = new Manager();
+    /**
+     * Arpit's Comments: 
+     * 1) Variables should be in camel case, HRServices is not a proper variable name - find out what better name you can give here. I think hrServices will be better.
+     */
     private final HRServices HRServices = new HRServices();
     private int empId;
     private String name;
@@ -52,6 +60,11 @@ public class EmployeeService {
         this.desig = desig;
     }
 
+    /**
+    * Arpit's Comments: 
+    * 1) The constructor should be placed before getters and setters. (Just a good practice)
+    * 2) Formatting should be done along with the coding, I can see many blank lines in methods.
+    */
     public EmployeeService(int empId, String name, double salary, String phone, String desig) {
         this.empId = empId;
         this.name = name;
@@ -65,6 +78,11 @@ public class EmployeeService {
         return accountService.updateSalary(salary);
     }
     public void eligibleForPromotion(String Perfect) {
+        /**
+        * Arpit's Comments: 
+        * 1) Method parameters should be in camel case.
+        * 2) Why this method is not reurning anything? Shouldn't it be returning a boolean value?
+        */
         HRServices.eligibleForPromotion(Perfect);
     }
     public  double calculateTDS(double salary) {
