d13de1765a205c9017b96efcea6627defdd7fe7f
arpitsaxena16 <30439383+arpitsaxena16@users.noreply.github.com>
2017-08-01 14:54:32 +0530

    Update CarBrandComparator.java

--- Day 6 Assignment/Day 6 (3 & 4 & 5) Assignment/src/CarBrandComparator.java
+++ Day 6 Assignment/Day 6 (3 & 4 & 5) Assignment/src/CarBrandComparator.java
@@ -2,6 +2,10 @@
  * Created by katkhedd on 7/25/2017.
  */
 import java.util.*;
+/**
+ * Arpit's Comments: 
+ * 1) Avoid using wild card in improts
+ */
 public class CarBrandComparator implements Comparator<Car>{
     public int compare(Car car1, Car car2){
         return car1.brand.compareTo(car2.brand);
