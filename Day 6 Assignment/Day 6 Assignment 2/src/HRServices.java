887c79f4c846cbd6dfdff51a7f186ac3e74e1b6d
arpitsaxena16 <30439383+arpitsaxena16@users.noreply.github.com>
2017-08-01 14:33:07 +0530

    Update HRServices.java

--- Day 6 Assignment/Day 6 Assignment 2/src/HRServices.java
+++ Day 6 Assignment/Day 6 Assignment 2/src/HRServices.java
@@ -3,6 +3,10 @@ public class HRServices {
     }
 
     public void eligibleForPromotion(String Perfect) {
+        /**
+         * Arpit's Comments: 
+         * 1) Method parameter names should be in camel case Perfect should be perfect
+         */
         if (Perfect.equals("Graduate traniee G")) {
             System.out.println("Eligible for promotion");
         }
@@ -12,7 +16,11 @@ public class HRServices {
     }
 
     public String changeDesignation(String desig) {
-
+        /**
+         * Arpit's Comments: 
+         * 1) Method parameter names should be proper and logical, like here we could have used designation in place of desig
+         * same for newDesig --> It should be newDesignation
+         */
         String newDesig = desig + "+";
         return newDesig;
     }
