b974b77d47216b9648fbc0091d2f712c1deff0ca
arpitsaxena16 <30439383+arpitsaxena16@users.noreply.github.com>
2017-08-02 14:41:47 +0530

    Update Cinema.java

--- Day 7 Assignment/src/Cinema.java
+++ Day 7 Assignment/src/Cinema.java
@@ -7,6 +7,12 @@ public class Cinema extends MovieService{
         User user1 = new User(1, "Ram", 18,4);
 
         try{
+            /**
+            * Arpit's Comments:
+            * Why should we extens the MoviService class? Is Cinema is of MovieService type?
+            * There is no IS-A relation between both, hence it should be a simple object inside it and using which these methods should be called.
+            * Please read about IS-A and HAS-A relationship in Java.
+            */
             buy(user1,2500);
             watch(user1);
 
